24e7133bdc2b0e75916c792c1588446d9b46ac6b
Mukesh97297 <mkverma97297@gmail.com>
2022-09-12 16:56:02 +0530

    navbar-logo

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,23 +1,67 @@
+import Link from 'next/link'
+import Document from 'next/document';
+import Image from 'next/image'
+
 import React from 'react'
+import gumtree from "../public/images/gumtree_logo.svg";
 function navbar() {
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
     return (
         <div>
-            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark" >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Logo</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-                        <span className="navbar-toggler-icon"></span>
+                    <Link href="/">
+                        <a className="navbar-brand">
+                            <Image src={gumtree} className="img-fluid"/>
+                            <span className='hisar-biz'>Hisar.biz</span>
+                        </a>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" >
+                        <span className="navbar-toggler-icon" onClick={openNav}></span>
                     </button>
                     <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                        <ul className="navbar-nav sidenav" id="mySidenav">
+                            <span className="closebtn" onClick={closeNav}>&times;</span>
+                            <li className="nav-item">                              
+                                <Link href="/"><a className="nav-link">Home</a></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                            <Link href="/"><a className="nav-link">Post on Ad</a></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                            <Link href="/"><a className="nav-link">Manage my Ads</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Messages</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Favourites</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">My Alerts</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Favourites</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">My Transaction</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">My Details</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Manage my Job Ads</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Help & Contact</a></Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Login / Register</a></Link>
                             </li>
                         </ul>
                     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Script from 'next/script';
-import styles from '../styles/Home.module.css';
-import Navbar from '../components/navbar';
+import Navbar from '../components/navbar'
 export default function Home() {
   return (
     <div>
