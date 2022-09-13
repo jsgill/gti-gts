37e6c3fbff9890b1081e326663a19175476da5e0
Mukesh97297 <mkverma97297@gmail.com>
2022-09-13 11:13:51 +0530

    navbar-changes

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Document from 'next/document';
 import Image from 'next/image'
 
 import React from 'react'
@@ -17,7 +16,7 @@ function navbar() {
                 <div className="container-fluid">
                     <Link href="/">
                         <a className="navbar-brand">
-                            <Image src={gumtree} className="img-fluid"/>
+                            <Image src={gumtree} className="img-fluid" />
                             <span className='hisar-biz'>Hisar.biz</span>
                         </a>
                     </Link>
@@ -27,41 +26,41 @@ function navbar() {
                     <div className="collapse navbar-collapse" id="collapsibleNavbar">
                         <ul className="navbar-nav sidenav" id="mySidenav">
                             <span className="closebtn" onClick={closeNav}>&times;</span>
-                            <li className="nav-item">                              
+                            <li className="nav-item">
                                 <Link href="/"><a className="nav-link">Home</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Post on Ad</a></Link>
+                                <Link href="/"><a className="nav-link">Post on Ad</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Manage my Ads</a></Link>
+                                <Link href="/"><a className="nav-link">Manage my Ads</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Messages</a></Link>
+                                <Link href="/"><a className="nav-link">Messages</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Favourites</a></Link>
+                                <Link href="/"><a className="nav-link">Favourites</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">My Alerts</a></Link>
+                                <Link href="/"><a className="nav-link">My Alerts</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Favourites</a></Link>
+                                <Link href="/"><a className="nav-link">Favourites</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">My Transaction</a></Link>
+                                <Link href="/"><a className="nav-link">My Transaction</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">My Details</a></Link>
+                                <Link href="/"><a className="nav-link">My Details</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Manage my Job Ads</a></Link>
+                                <Link href="/"><a className="nav-link">Manage my Job Ads</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Help & Contact</a></Link>
+                                <Link href="/"><a className="nav-link">Help & Contact</a></Link>
                             </li>
                             <li className="nav-item">
-                            <Link href="/"><a className="nav-link">Login / Register</a></Link>
+                                <Link href="/"><a className="nav-link">Login / Register</a></Link>
                             </li>
                         </ul>
                     </div>
