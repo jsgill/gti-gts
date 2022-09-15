8f57096e12b08be123b7791204d5455e3d352de3
Mukesh97297 <mkverma97297@gmail.com>
2022-09-15 08:33:55 +0530

    navbar logo

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,8 @@
 import Link from 'next/link'
 import Image from 'next/image'
-
 import React from 'react'
 import gumtree from "../public/images/gumtree_logo.svg";
 function navbar() {
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-    }
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-    }
     return (
         <div>
             <nav className="navbar navbar-expand-sm bg-dark navbar-dark" >
@@ -17,50 +10,33 @@ function navbar() {
                     <Link href="/">
                         <a className="navbar-brand">
                             <Image src={gumtree} className="img-fluid" />
-                            <span className='hisar-biz'>Hisar.biz</span>
-                        </a>
+                            <span className='hisar-biz'>Hisar.biz</span></a>
                     </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" >
-                        <span className="navbar-toggler-icon" onClick={openNav}></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                        <ul className="navbar-nav sidenav" id="mySidenav">
-                            <span className="closebtn" onClick={closeNav}>&times;</span>
+                        <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Home</a></Link>
+                                <Link href="/"><a className="nav-link">Cars & Vehicles</a></Link>
                             </li>
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Post on Ad</a></Link>
+                                <Link href="/"><a className="nav-link">Home & Garden</a></Link>
                             </li>
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Manage my Ads</a></Link>
+                                <Link href="/"><a className="nav-link">Jobs</a></Link>
                             </li>
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Messages</a></Link>
+                                <Link href="/"><a className="nav-link">Real Estate</a></Link>
                             </li>
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Favourites</a></Link>
+                                <Link href="/"><a className="nav-link">Services For Hire</a></Link>
                             </li>
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">My Alerts</a></Link>
+                                <Link href="/"><a className="nav-link">Value My Car</a></Link>
                             </li>
                             <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Favourites</a></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/"><a className="nav-link">My Transaction</a></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/"><a className="nav-link">My Details</a></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Manage my Job Ads</a></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Help & Contact</a></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/"><a className="nav-link">Login / Register</a></Link>
+                                <Link href="/"><a className="nav-link">More...</a></Link>
                             </li>
                         </ul>
                     </div>
@@ -69,5 +45,4 @@ function navbar() {
         </div>
     )
 }
-
 export default navbar
